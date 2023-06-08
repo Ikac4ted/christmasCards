f4ae51b5f29c0a2a81e60d2ecf700897c2663542
Ikac4ted <132400509+Ikac4ted@users.noreply.github.com>
2023-06-08 18:17:10 +0200

    Add files via upload

---
+++ christmasCards/christmas.py
@@ -0,0 +1,19 @@
+import os
+
+def create_christmas_cards():
+    with open('employees.txt', 'r') as names:
+        employees = names.read().splitlines()
+        
+    with open('template.txt', 'r') as template_file:
+        template = template_file.read()
+
+    if not os.path.exists('christmasCards'):
+        os.makedirs('christmasCards')
+
+    for employee in employees:
+        message = template.replace('NAME', employee)
+        with open(f'christmasCards/{employee}.txt', 'w') as card:
+            card.write(message)
+
+if __name__ == "__main__":
+    create_christmas_cards()
